0313514ed7bdf163bfe8611b083b2c2c90bb8ce0
imtripleq <flamework89@hotmail.com>
2021-11-08 10:25:09 +1100

    completed c

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 class Formatter {
   //add static methods here
+  static capitalize(e) {
+    return e.charAt(0).toUpperCase() + e.slice(1);
+  }
+
+  static sanitize(e) {
+    return e.replace(/[^A-Za-z0-9 '-]/g, "");
+  }
+
+  static titleize(e) {
+    const noWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+
+    const answer = [];
+    const newArray = e.split(" ");
+    for (let i = 0; i < newArray.length; i++) {
+      if (i === 0) {
+        answer.push(this.capitalize(newArray[i]));
+      } else {
+        if (noWords.includes(newArray[i])) {
+          answer.push(newArray[i]);
+        } else {
+          answer.push(this.capitalize(newArray[i]));
+        }
+      }
+    }
+    return answer.join(" ");
+  }
 }
